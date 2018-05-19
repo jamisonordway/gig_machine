01d0856a67755991ce0abaf0e269abe39b7af3ca
JamisonOrdway <jamisonordway@gmail.com>
2018-05-19 15:11:23 -0600

    Adds views for events and venues

--- spec/features/user_can_see_homepage_spec.rb
+++ spec/features/user_can_see_homepage_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 describe 'Visitor' do
   scenario 'they visit the homepage' do
     visit dashboard_index_path
-
+    save_and_open_page
     expect(page).to have_content("Welcome to the Gig Machine!")
+    expect(page).to have_link('Click here to see all venues')
+    expect(page).to have_link('Click here to see all events')
   end
 end
