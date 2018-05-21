4bccdf3543e96826014247738579c96c78df25bc
JamisonOrdway <jamisonordway@gmail.com>
2018-05-21 15:22:55 -0600

    Implements home page, TESTS sign up and login

--- spec/features/user_can_see_homepage_spec.rb
+++ spec/features/user_can_see_homepage_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 describe 'Visitor' do
   scenario 'they visit the homepage' do
+    visit '/'
+
+    expect(page).to have_link('Log in')
+    expect(page).to have_link('Sign up')
+    expect(page).to have_link('Gig Machine Dashboard')
+  end
+  scenario 'they visit the dashboard' do
     venue = Venue.create!(name: 'The Prelude', location: 'Harlingen', email: 'Angel@music.com')
     event = venue.events.create(title: 'Songwriters in the Round', description: 'we will sing each others songs', date: '12/12/16')
     visit dashboard_index_path
