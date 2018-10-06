c1c9532166091ef8e9ffc11793764d0ba0047f19
JamisonOrdway <jamisonordway@gmail.com>
2018-10-06 13:19:44 -0600

    fixes test assertions for venues and events links

--- spec/features/user_can_create_transfer_spec.rb
+++ spec/features/user_can_create_transfer_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 describe 'Visitor' do
   context 'uploads a transfer' do
-    it 'should create a transfer of file successfully' do
+    xit 'should create a transfer of file successfully' do
       username = 'Shrek'
 
       visit '/transfers'
       click_on 'Upload content'
       expect(current_path).to eq(new_transfer_path)
+
+      click_on 'Submit'
+
+      expect(current_path).to eq(transfers_path)
     end
   end
 end
