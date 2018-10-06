0b5ef60a2448a2e140b7a4b116836cb760bb1d99
JamisonOrdway <jamisonordway@gmail.com>
2018-10-06 15:17:00 -0600

    Adds figaro for WT_API_KEY environment variable

--- Gemfile
+++ Gemfile
@@ -34,7 +34,9 @@ gem 'jbuilder', '~> 2.5'
 gem 'administrate'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
-gem 'wetransfer'
+gem 'wetransfer', '~> 0.9.0.beta2'
+gem 'figaro'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,9 +1,11 @@
+require 'we_transfer_client'
+# require '.env'
+# Dotenv.load 
 
 class TransfersController < ApplicationController
-  require 'we_transfer_client'
   
   def new
-    client = WeTransfer::Client.new(api_key: ENV.fetch('WT_API_KEY'))
+    client = WeTransfer::Client.new(api_key: ENV['WT_API_KEY'])
     transfer = client.create_transfer_and_upload_files(message: 'Performances') do |upload|
       upload.add_file_at(path: '/path/to/local/file.jpg')
       upload.add_file_at(path: '/path/to/another/local/file.jpg')
